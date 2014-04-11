f6313fc0906a605af768865ac03300c8463587bf
lmgeorge <kote1010@gmail.com>
2014-04-11 14:12:49 -0400

    refactored with readValue method

--- code/units/JavaBasics/Lesson03/WindowMaster/WindowMaster/src/windowmaster/WindowMaster.java
+++ code/units/JavaBasics/Lesson03/WindowMaster/WindowMaster/src/windowmaster/WindowMaster.java
@@ -1,5 +1,7 @@
 package windowmaster;
+
 import java.util.Scanner;
+
 /**
  *
  * @author apprentice
@@ -13,53 +15,37 @@ public class WindowMaster {
 // Calculate total cost = glass + trim
 // Need area of glass and trim, all floats
   public static void main(String[] args) {
+    // Predetermined constants
     final double MAX_HEIGHT = 25.5;
     final double MAX_WIDTH = 18.75;
     final double MIN_HEIGHT = 1.0;
     final double MIN_WIDTH = 1.0;
-      
-    //Grab user input
-    Scanner unit = new Scanner(System.in);
-    
-    //Grab all float variables of the window
-    float height, 
-          width, 
-          cost, 
-          perimeterWindow, 
-          areaWindow,
-          glassCost,
-          trimCost;
+    final double MAX_COST = 10000;
+    final double MIN_COST = 0;
 
-             
+    //Grab all variables of the window
+    double  height,
+            width,
+            cost,
+            perimeterWindow,
+            areaWindow,
+            glassCost,
+            trimCost;
     
-    do {
-        System.out.println("Please enter window height between 1 and 25.5:");
-        height = Float.parseFloat(unit.nextLine());
-        if (height <= MIN_HEIGHT || height >= MAX_HEIGHT){
-            System.out.println("Error: Please enter a height between 1 and 25.\n");
-        }
-    }while (height <= MIN_HEIGHT || height >= MAX_HEIGHT);
-    
-    
-    do {
-        System.out.println("Please enter window width 1 and 18.75:");
-        width = Float.parseFloat(unit.nextLine());
-        if (width <= MIN_WIDTH || width >= MAX_WIDTH){
-            System.out.println("Error: Please enter a width between 1 and 18.75.\n");
-        }
-    }while (width <= MIN_WIDTH || width >= MAX_WIDTH);    
-    
-    System.out.println("Please enter the cost of glass per square inch.");
-    glassCost = Float.parseFloat(unit.nextLine());
-    
-    System.out.println("Please enter the cost of trim per linear inch.");
-    trimCost = Float.parseFloat(unit.nextLine());
-    
-    
+    //Get measures
+    height = readValue("Please enter window height:", MAX_HEIGHT, MIN_HEIGHT);
+    width = readValue("Please enter window width:", MAX_WIDTH, MIN_WIDTH);
+
+    //Get cost
+    glassCost = readValue("Please enter the cost of glass per square inch.", MAX_COST, MIN_COST);
+    trimCost = readValue("Please enter the cost of trim per linear inch.", MAX_COST, MIN_COST);
+
+    //Calculate area, perimeter, and total cost of window
     areaWindow = height * width;
     perimeterWindow = 2 * (height * width);
     cost = (glassCost * areaWindow + trimCost * perimeterWindow);
 
+    //Print all measures, calculations, and total cost of window
     System.out.println(
       "Window height = " + height + "\n"
       + "Window width = " + width + "\n"
@@ -69,5 +55,20 @@ public class WindowMaster {
     );
 
   }
-  
+
+  public static double readValue(String prompt, double max, double min) {
+    Scanner ui = new Scanner(System.in);
+    System.out.println(prompt);
+    double value;
+
+    do {
+      value = ui.nextFloat();
+      if (value < min || value > max) {
+        System.out.println("Error: Please enter a value between" + min + " and " + max + "\n");
+      }
+    } while (value < min || value > max);
+
+    return value;
+  }
+
 }
