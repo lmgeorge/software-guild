88f2c6a67beb2e8ea43c25ddf16a088aaf2bf371
lmgeorge <kote1010@gmail.com>
2014-04-15 18:22:33 -0400

    Factorizor refactored (Lab 02)

--- code/units/JavaBasics/Lesson05_ControllingProgramFlow/Factorizor/src/factorizor/Factorizor.java
+++ code/units/JavaBasics/Lesson05_ControllingProgramFlow/Factorizor/src/factorizor/Factorizor.java
@@ -5,7 +5,7 @@
  */
 package factorizor;
 
-import java.util.Scanner;
+
 
 /**
  *
@@ -13,48 +13,15 @@ import java.util.Scanner;
  */
 public class Factorizor {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
 
-        long sumFactors = 0, prime = 0, initial;
-        do {
-            System.out.println("What number would you like to factor?");
-            initial = sc.nextLong();
+    FactorizorMachine calculator = new FactorizorMachine();
 
-            if (initial <= 0) {
-                System.out.println("Error: Enter a positive whole number.");
-            }
+    calculator.findPrimes();
 
-        } while (initial <= 0);
-
-        if (initial == 1) {
-            System.out.println("One has no factors.  It's special.");
-        } else {
-            System.out.println("The factors of " + initial + " are:");
-        }
-
-        for (int i = 1; i < initial; i++) {
-            if (initial % i == 0) {
-                System.out.println(i);
-                sumFactors += i;
-                prime++;
-            }//end if
-        }//end for
-
-        if (sumFactors == initial) {
-            System.out.println(initial + " is a perfect number");
-        }
-        if (prime == 1) {
-
-            System.out.println(initial + " is a prime number");
-        } else {
-            System.out.println(initial + " is not a prime number");
-
-        }
-
-    }
+  }
 
 }
