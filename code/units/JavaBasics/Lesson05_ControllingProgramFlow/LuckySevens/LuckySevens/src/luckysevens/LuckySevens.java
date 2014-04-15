06626d12649c7b2a62428921bbf296a365c4c4ff
lmgeorge <kote1010@gmail.com>
2014-04-15 15:57:30 -0400

    finished Lab 02: LuckySevens refactor

--- code/units/JavaBasics/Lesson05_ControllingProgramFlow/LuckySevens/LuckySevens/src/luckysevens/LuckySevens.java
+++ code/units/JavaBasics/Lesson05_ControllingProgramFlow/LuckySevens/LuckySevens/src/luckysevens/LuckySevens.java
@@ -5,9 +5,6 @@
  */
 package luckysevens;
 
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  *
  * @author apprentice
@@ -18,36 +15,43 @@ public class LuckySevens {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    int roll1, roll2, rollCounter = 0, betLimit, maxAmountBet = 0, maxAmountRoll = 0;
-    Random rGen = new Random();
-    Scanner sc = new Scanner(System.in);
-
-    System.out.print("How much money do you want to bet? ");
-    betLimit = sc.nextInt();
     
-    // luckySevens.play(betLimit); //game code should stop here
+      LuckySevensGameEngine game = new LuckySevensGameEngine();
+      game.play();
+      
     
-   //Move everything to new class
-    maxAmountBet = betLimit;
-    if (betLimit > 0) {
-      while (betLimit > 0) {
-        roll1 = rGen.nextInt(6) + 1;
-        roll2 = rGen.nextInt(6) + 1;
-        rollCounter++;
-        if (roll1 + roll2 == 7) {
-          betLimit += 4;
-          if (betLimit > maxAmountBet) {
-            maxAmountBet = betLimit;
-            maxAmountRoll = rollCounter;
-          }
-        } else {
-          betLimit -= 1;
-        }
-      }
-      System.out.println("You are broke after " + rollCounter + " rolls.");
-      System.out.println("You should have quit after " + maxAmountRoll + " rolls, when you had " + maxAmountBet + ".");
-    } else {
-      System.out.println("Can't play!");
+    
+//    int roll1, roll2, rollCounter = 0, betLimit, maxAmountBet = 0, maxAmountRoll = 0;
+//    Random rGen = new Random();
+//    Scanner sc = new Scanner(System.in);
+//
+//    System.out.print("How much money do you want to bet? ");
+//    betLimit = sc.nextInt();
+//    
+//    // luckySevens.play(betLimit); //game code should stop here
+//    
+//   //Move everything to new class
+//    maxAmountBet = betLimit;
+//    if (betLimit > 0) {
+//      while (betLimit > 0) {
+//        roll1 = rGen.nextInt(6) + 1;
+//        roll2 = rGen.nextInt(6) + 1;
+//        rollCounter++;
+//        if (roll1 + roll2 == 7) {
+//          betLimit += 4;
+//          if (betLimit > maxAmountBet) {
+//            maxAmountBet = betLimit;
+//            maxAmountRoll = rollCounter;
+//          }
+//        } else {
+//          betLimit -= 1;
+//        }
+//      }
+//      System.out.println("You are broke after " + rollCounter + " rolls.");
+//      System.out.println("You should have quit after " + maxAmountRoll + " rolls, when you had " + maxAmountBet + ".");
+//    } else {
+//      System.out.println("Can't play!");
+    
     }
-  }
+
 }
