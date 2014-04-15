8c84479e3c66bc22fc713c087a613b1f5c9ff7e0
lmgeorge <kote1010@gmail.com>
2014-04-15 18:21:05 -0400

    initial commit

---
+++ Printer/src/printer/MyPrinter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package printer;
+
+/**
+ *
+ * @author lmgeorge <lauren.george@live.com>
+ */
+public class MyPrinter {
+  public void println(String str){
+    System.out.println(str);
+  }
+  public void println(int num){
+    System.out.println(num);
+  }
+  public void println(){
+    System.out.println();
+  }
+  public void println(Float floating){
+    System.out.println(floating);
+  }
+  public void println(char c){
+    System.out.println(c);
+  }
+}
