53f5ee657d00b874fb41ce913faea48e04005972
lmgeorge <kote1010@gmail.com>
2014-06-11 17:19:13 -0400

    still updating this internal utility

--- Lab/src/main/java/io/digitalpurple/lab/SqlWriter.java
+++ Lab/src/main/java/io/digitalpurple/lab/SqlWriter.java
@@ -22,81 +22,115 @@ import java.util.logging.Logger;
  * @author lmgeorge <lauren.george@live.com>
  */
 public class SqlWriter {
-	private String industries = "`industries`";
-	private String countries = "`countries`";
-	private String states = "`states`";
-	private String cities = "`cities`";
-	
-	private String countriesFile = 
-		"/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/list_of_countries.txt";
-	private String countriesSql = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/countries.sql";
-	
-	private String industriesFile = "";
-	private String industriesSql = "";
-	
+
+	private static final String industriesTable = "`industries`";
+	private static final String tableName = "`countries`";
+	private static final String statesTable = "`states`";
+	private static final String citiesTable = "`cities`";
+
+	private static final String countriesFile
+		= "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/list_of_countries.txt";
+	private static final String countriesSql = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/countries.sql";
+
+	private static final String industriesFile = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/list_of_industries.txt";
+	private static final String industriesSql = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/industries.sql";
+
+	private static final String statesFile = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/list_of_state_abbrs.txt";
+	private static final String statesSql = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/states.sql";
+
+	private final String MSA_CITIES = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/msaCities.txt";
+	private final String STATES_ABBR = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/statename_stateabbr.txt";
+	private final String CITIES_SQL = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/cities.sql";
+	private static final String usingStmts = "USE STARSYSTEM_StudentsDB;\n";
+	private static final String usingStmtTest = "\nUSE STARSYSTEM_StudentsDB_Test;\n";
+	private static final String deleteStmts = "DELETE FROM %s;\n\n";
+	private static final String auto_increment = "ALTER TABLE %s auto_increment = 1;\n\n";
+
 	public static void main(String[] args) throws IOException {
 		SqlWriter sw = new SqlWriter();
 
-		sw.countriesSQL();
-		Map<Integer, String> map = sw.statesSQL();
-		sw.industriesSQL();
-
-		sw.mapCityToStateId(sw.getStatesFromDB(map));
+		sw.singleValueInsertSQL("`country`", "`countries`", countriesFile, countriesSql);
+//		Map<Integer, String> map = sw.statesSQL();
+//		sw.industriesSQL();
+//
+//		sw.mapCityToStateId(sw.getStatesFromDB(map));
+//		String cote = "Cote d' Ivoire".replaceAll("[']", "\\\\'");
+//		System.out.println(cote);
 	}
 
-	public void countriesSQL() {
+	public void singleValueInsertSQL(String colName, String tableName, String inFile, String outFile) {
 		try {
-			Scanner countries = new Scanner(
-				new BufferedReader(new FileReader(countriesFile)));
-			PrintWriter writer = new PrintWriter(new FileWriter(countriesSql));
+			Scanner data = new Scanner(new BufferedReader(new FileReader(inFile)));
+			Scanner dataTest = new Scanner(new BufferedReader(new FileReader(inFile)));
+			PrintWriter writer = new PrintWriter(new FileWriter(outFile));
 
-			writer.println("USE STARSYSTEM_StudentsDB;");
-			writer.println("USE STARSYSTEM_StudentsDB_Test;\n");
-			writer.println("DELETE FROM `countries`;");
-			writer.println("ALTER TABLE `countries` auto_increment = 1;\n");
+			//Inserts for production database
+			writer.println(usingStmts);
+			writer.printf(deleteStmts, tableName);
+			writer.printf(auto_increment, tableName);
 
-			while (countries.hasNextLine()) {
-				String country = countries.nextLine();
-				String insertCountry = "INSERT INTO `countries`(`country`) value (\"" + country + "\");";
+			while (data.hasNextLine()) {
+				String datum = data.nextLine();
 
-				writer.println(insertCountry);
+				writer.printf("INSERT INTO %s(%s) value (\"%s\");\n", tableName, colName, datum);
+				writer.flush();
+			}
+
+			//Inserts for test database
+			writer.println(usingStmtTest);
+			writer.printf(deleteStmts, tableName);
+			writer.printf(auto_increment, tableName);
+
+			while (dataTest.hasNextLine()) {
+				String datumTest = dataTest.nextLine();
+
+				writer.printf("INSERT INTO %s(%s) value (\"%s\");\n", tableName, colName, datumTest);
 				writer.flush();
 			}
 
 			writer.close();
-		} catch (FileNotFoundException ex) {
-			Logger.getLogger(SqlWriter.class.getName()).log(Level.SEVERE, null, ex);
+
 		} catch (IOException ex) {
 			Logger.getLogger(SqlWriter.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 
-	public Map<Integer, String> statesSQL() {
+	public Map<Integer, String> singleValueInsertMapSQL(String colName, String tableName, String inFile, String outFile) {
 		Map<Integer, String> map = new HashMap<>();
 		int ctr = 1;
 		try {
-			Scanner states = new Scanner(
-				new BufferedReader(new FileReader("/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/list_of_state_abbrs.txt")));
-			PrintWriter writer = new PrintWriter(new FileWriter("/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/states.sql"));
+			Scanner data = new Scanner(new BufferedReader(new FileReader(inFile)));
+			Scanner dataTest = new Scanner(new BufferedReader(new FileReader(inFile)));
+			PrintWriter writer = new PrintWriter(new FileWriter(outFile));
 
-			writer.println("USE STARSYSTEM_StudentsDB;");
-			writer.println("USE STARSYSTEM_StudentsDB_Test;\n");
+			//Inserts for production database
+			writer.println(usingStmts);
+			writer.printf(deleteStmts, tableName);
+			writer.printf(auto_increment, tableName);
 
-			writer.println("DELETE FROM `states`;");
-			writer.println("ALTER TABLE `states` auto_increment = 1;\n");
-
-			while (states.hasNextLine()) {
-				String state = states.nextLine();
-				String insertState = "INSERT INTO `states`(`state`) value (\"" + state + "\");";
-				map.put(ctr, state);
+			while (data.hasNextLine()) {
+				String datum = data.nextLine();
+				writer.printf("INSERT INTO %s(%s) value (\"%s\");\n", tableName, colName, datum);
+				
+				map.put(ctr, datum);
 				ctr++;
-				writer.println(insertState);
+				
+				writer.flush();
+			}
+
+			//Inserts for test database
+			writer.println(usingStmtTest);
+			writer.printf(deleteStmts, tableName);
+			writer.printf(auto_increment, tableName);
+
+			while (dataTest.hasNextLine()) {
+				String datumTest = dataTest.nextLine();
+
+				writer.printf("INSERT INTO %s(%s) value (\"%s\");\n", tableName, colName, datumTest);
 				writer.flush();
 			}
 
 			writer.close();
-		} catch (FileNotFoundException ex) {
-			Logger.getLogger(SqlWriter.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (IOException ex) {
 			Logger.getLogger(SqlWriter.class.getName()).log(Level.SEVERE, null, ex);
 		}
@@ -107,14 +141,14 @@ public class SqlWriter {
 	public void industriesSQL() {
 		try {
 			Scanner industries = new Scanner(
-				new BufferedReader(new FileReader("/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/list_of_industries.txt")));
-			PrintWriter writer = new PrintWriter(new FileWriter("/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/industries.sql"));
+				new BufferedReader(new FileReader(industriesFile)));
+			PrintWriter writer = new PrintWriter(new FileWriter(industriesSql));
 
 			writer.println("USE STARSYSTEM_StudentsDB;");
-			writer.println("USE STARSYSTEM_StudentsDB_Test;\n");
+			writer.println("USE STARSYSTEM_StudentsDB_Test;\n\n");
 
 			writer.println("DELETE FROM `industries`;");
-			writer.println("ALTER TABLE `industries` auto_increment = 1;\n");
+			writer.println("ALTER TABLE `industries` auto_increment = 1;\n\n");
 
 			while (industries.hasNextLine()) {
 				String industry = industries.nextLine();
@@ -132,13 +166,10 @@ public class SqlWriter {
 		}
 	}
 
-	private final String MSA_CITIES = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/msaCities.txt";
-	private final String STATES_ABBR = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataTxt/statename_stateabbr.txt";
-	private final String CITIES_SQL = "/home/apprentice/Documents/GitHub/Java-April2014-Group-Capstone/DBFiles/refDataSql/cities.sql";
 
 	public Map<String, Integer> getStatesFromDB(Map<Integer, String> allStates) throws FileNotFoundException, IOException {
 
-			//Read in the State abbreviations file and map with an id from DB
+		//Read in the State abbreviations file and map with an id from DB
 		//<StateName, Order in File>
 		Scanner sc1 = new Scanner(new BufferedReader(new FileReader(STATES_ABBR)));
 		Map<String, Integer> stateNames = new HashMap<>();
@@ -163,7 +194,7 @@ public class SqlWriter {
 	public void mapCityToStateId(Map<String, Integer> mappedStates) throws FileNotFoundException {
 		Scanner sc = new Scanner(new BufferedReader(new FileReader(MSA_CITIES)));
 		PrintWriter writer = new PrintWriter(CITIES_SQL);
-		
+
 		writer.println("USE STARSYSTEM_StudentsDB;");
 		writer.println("USE STARSYSTEM_StudentsDB_Test;\n");
 
